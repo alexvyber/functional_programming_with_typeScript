d653b2d701dae9930156fbc9bc9993074cca6a98
Alexey Sokolov <alexvyber@gmail.com>
2023-02-09 19:15:23 +0300

    new stuff

--- Chapter02/08_specialized_overloading_signature.ts
+++ Chapter02/08_specialized_overloading_signature.ts
@@ -1,10 +1,8 @@
-namespace specialized_overloading_signature_demo_1 {
-
-    interface Document {
-        createElement(tagName: "div"): HTMLDivElement; // specialized
-        createElement(tagName: "span"): HTMLSpanElement; // specialized
-        createElement(tagName: "canvas"): HTMLCanvasElement; // specialized
-        createElement(tagName: string): HTMLElement; // non-specialized
-    }
-    
+interface Document {
+  createElement(tagName: "div"): HTMLSpanElement; // specialized
+  createElement(tagName: "span"): HTMLDivElement; // specialized
+  createElement(tagName: "canvas"): HTMLCanvasElement; // specialized
+  createElement(tagName: string): HTMLElement; // non-specialized
 }
+
+const div = document.createElement("div");
